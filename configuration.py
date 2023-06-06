cc78d503d7681b81c2afa1632ff8218f5245155d
Sovereign007 <124557254+Sovereign007@users.noreply.github.com>
2023-06-06 15:17:37 +0300

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://7f96877a-db6f-4cd7-a4ed-ff5c8996da20.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_SEARCH = "/api/v1/products/kits"
